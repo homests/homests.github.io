3ad46931dd4b11d1db82f8a3ac027d585b091abd
EduardoArtioli <artioli1@hotmail.com>
2021-09-21 15:27:44 -0300

    Updates

---
+++ precache-manifest.72390539d41919c9cb96bd342c67b756.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "918b154895afe80ef102",
+    "url": "/static/css/main.fec84667.chunk.css"
+  },
+  {
+    "revision": "918b154895afe80ef102",
+    "url": "/static/js/main.918b1548.chunk.js"
+  },
+  {
+    "revision": "20bcc85fc5ffed52150b",
+    "url": "/static/js/1.20bcc85f.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "66dde39c1cb904a28a4caa6868d18979",
+    "url": "/index.html"
+  }
+];
